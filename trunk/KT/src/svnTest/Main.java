53176d9457cba986a31a4a27836b0bb2000873c3
fabiantheblind <fabiantheblind@f76a2f8a-fb08-11de-acd2-a780054a256f>
2010-01-29 07:46:45 +0000

    How to load PShape. Unfortunatly it doesnt work in eclipse right now, but should work in Processing. The sketch cant reach the data folder…

--- trunk/KT/src/svnTest/Main.java
+++ trunk/KT/src/svnTest/Main.java
@@ -1,32 +1,47 @@
 package svnTest;
 
 import processing.core.*;
-
+// check this link in the processing reference for all PSahpe functions
+// http://processing.org/reference/PShape.html
 @SuppressWarnings("serial")
 public class Main extends PApplet {
 	
-	PImage img = new PImage();
-	PImage mask;
+//	PImage img = new PImage();
+//	PImage mask;
+	PShape mySVG;
 	
 	public void setup(){
 		size(800,800);
-		img = loadImage("../resource/studio.jpg");
-		mask = new PImage(img.width,img.height); 
-		mask.format = ALPHA;
-		for(int x=0;x<mask.width/2;x++){
-			for(int y=0;y<mask.height/2;y++){
-				mask.pixels[y*mask.width + x] = 255;
-			}
-		}
+		mySVG = loadShape("tmktSvgTest.svg");
+
+//		img = loadImage("../resource/studio.jpg");
+//		mask = new PImage(img.width,img.height); 
+//		mask.format = ALPHA;
+//		for(int x=0;x<mask.width/2;x++){
+//			for(int y=0;y<mask.height/2;y++){
+//				mask.pixels[y*mask.width + x] = 255;
+//			}
+//		}
 	}
 	
 	public void draw(){
 		background(0,0,0);
-		ellipse(50, 50, 30, 20);
-		ellipse(150, 50, 30, 20);
+		mySVG.disableStyle();
+		shapeMode(CENTER);
 		
-		img.mask(mask.pixels);
-		image(img,0,0);
+		fill(255,0,0);
+		shape(mySVG);
+		fill(255,0,0,200);
+		shape(mySVG,5,5);
+		fill(255,0,0,100);
+		shape(mySVG,23,23,500,500);
+		mySVG.enableStyle();
+
+		
+//		ellipse(50, 50, 30, 20);
+//		ellipse(150, 50, 30, 20);
+//		img.mask(mask.pixels);
+//		image(img,0,0);
 		
 	}
 }
