d4215c8a9c3fe9f0bd527a37003ba026987b138f
fabiantheblind <fabiantheblind@f76a2f8a-fb08-11de-acd2-a780054a256f>
2010-01-29 08:34:35 +0000

    Conus Fires now changed to work like Weather stations. But where do i initilaize them? Tried to but they dont show

---
+++ trunk/KT/src/layer/ConusFire.java
@@ -0,0 +1,50 @@
+package layer;
+
+
+import java.util.ArrayList;
+
+import processing.core.PApplet;
+import utils.Container;
+
+import com.modestmaps.core.Point2f;
+import com.modestmaps.geo.Location;
+
+public class ConusFire{
+	
+	public PApplet p;
+	public String discrStr, styleUrlStr;
+	public Location location;
+	
+	
+	ConusFire(PApplet p,float cLat,float cLon, String cDiscr,String cStyleUrl){
+		this(p,new Location(cLat,cLon),cDiscr,cStyleUrl);
+	}
+
+	ConusFire(PApplet p,Location cLocation, String cDiscr,String cStyleUrl){
+		location = cLocation;
+		discrStr = cDiscr;
+		styleUrlStr = cStyleUrl;
+		this.p = p;
+	}
+	
+	
+	  public void drawFire(ArrayList<Container> listener){
+		   	for(Container container : listener){
+	    		Point2f point = container.locationPoint(location);
+	    		if(!container.isInside((int)point.x, (int)point.y)){
+	    			continue;
+	    		}
+		p.smooth();
+	p.fill(255);
+	p.stroke(0);
+//	Point2f myPoint = map.locationPoint(location);
+	p.ellipse(point.x, point.y, 4, 4);
+	float tw = p.textWidth(styleUrlStr);
+	p.rect(point.x + 9, point.y - 21, tw + 6 , 18);
+	p.line(point.x, point.y, point.x + 8, point.y - 3);
+	p.fill(0);
+	p.text(styleUrlStr, point.x + 11, point.y - 8);
+	}
+	
+	}
+}
