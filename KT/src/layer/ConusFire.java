1a138036b2fcb94d6edebd574382b9992fed69aa
fabiantheblind <fabiantheblind@f76a2f8a-fb08-11de-acd2-a780054a256f>
2010-02-01 17:55:03 +0000

    styles?

--- KT/src/layer/ConusFire.java
+++ KT/src/layer/ConusFire.java
@@ -1,24 +1,28 @@
 package layer;
-
-
 import java.util.ArrayList;
 
 import processing.core.PApplet;
+import processing.core.PShape;
 import utils.Container;
+import utils.Styles;
 
 import com.modestmaps.core.Point2f;
 import com.modestmaps.geo.Location;
 
 public class ConusFire{
-	
+	public Styles myStyle;
 	public PApplet p;
 	public String discrStr, styleUrlStr;
 	public Location location;
 //	public Style fireStyle;
+	PShape myFireIcon = p.loadShape("../data/fire.svg");
 	
+//	Styles.createColors();
 	
 	ConusFire(PApplet p,float cLat,float cLon, String cDiscr,String cStyleUrl){
 		this(p,new Location(cLat,cLon),cDiscr,cStyleUrl);
+
+
 	}
 
 	ConusFire(PApplet p,Location cLocation, String cDiscr,String cStyleUrl){
@@ -30,6 +34,8 @@ public class ConusFire{
 	
 	
 	  public void drawFire(ArrayList<Container> listener){
+		//	myFireIcon = p.loadShape("../data/fire.svg");
+
 		   	for(Container container : listener){
 	    		Point2f point = container.locationPoint(location);
 	    		if(!container.isInside((int)point.x, (int)point.y)){
@@ -40,7 +46,12 @@ public class ConusFire{
 //	p.stroke(0);
 		p.noStroke();
 //	Point2f myPoint = map.locationPoint(location);
-	p.ellipse(point.x, point.y, 10, 10);
+		p.shapeMode(p.CENTER);
+		myFireIcon.disableStyle();
+		p.fill(5,5,5,250);
+		p.shape(myFireIcon,point.x,point.y,11,11);
+
+//	p.ellipse(point.x, point.y, 10, 10);
 	//float tw = p.textWidth(styleUrlStr);
 //	p.rect(point.x + 9, point.y - 21, tw + 6 , 18);
 //	p.line(point.x, point.y, point.x + 8, point.y - 3);
